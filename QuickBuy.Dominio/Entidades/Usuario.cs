83791e71bba18c0179ca9bc8848eae52dd8fd813
matheuscsmg <matheusds.sousa@gmail.com>
2021-02-08 15:57:30 +0000

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        /// <summary>
+        ///Um Usuario pode ter nenhum ou muitos pedidos
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
