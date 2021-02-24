97d58008dca0d6252797e03af2cdf878beed857f
matheuscsmg <matheusds.sousa@gmail.com>
2021-02-24 17:33:17 +0000

    configurando rotas entre componentes do Angular

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,20 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public usuario;
+  public returnUrl: string;
 
-  constructor() {
-    this.usuario = new Usuario();
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
   }
+
+    ngOnInit(): void {
+      this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+      this.usuario = new Usuario();
+    }
 
   entrar() {
     if (this.usuario.email == "matheusds.sousa@gmail.com" && this.usuario.senha == "123") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
     }
   }
 
